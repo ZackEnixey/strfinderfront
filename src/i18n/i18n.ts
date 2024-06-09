1a01c320abf2a271bea71d2df2ac51e073c4b9a1
ZackEnixey <zoran.zilic.zack@gmail.com>
2024-06-09 17:32:29 +0200

    feature: add the lanugages and add some examples to a few of components

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -2,15 +2,29 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import en from "./translations/en.json";
+import es from "./translations/es.json";
 import fr from "./translations/fr.json";
-import he from "./translations/he.json";
+import it from "./translations/it.json";
+import de from "./translations/de.json";
 import sr from "./translations/sr.json";
+import ar from "./translations/ar.json";
+import ru from "./translations/ru.json";
+import he from "./translations/he.json";
+import zh from "./translations/zh.json";
+import ja from "./translations/ja.json";
 
 const resources = {
   en: { translation: en },
+  es: { translation: es },
   fr: { translation: fr },
+  it: { translation: it },
+  de: { translation: de },
+  sr: { translation: sr },
+  ar: { translation: ar },
+  ru: { translation: ru },
   he: { translation: he },
-  sr: { translation: sr }
+  zh: { translation: zh },
+  ja: { translation: ja }
 };
 
 export const availableLanguages = Object.keys(resources);
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx';
 import "./scss/main.scss";
 import GlobalContextProvider from './context/GlobalContextProvider.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import "./i18n/i18n";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
